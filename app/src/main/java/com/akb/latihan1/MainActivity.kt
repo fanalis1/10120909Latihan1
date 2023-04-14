7ce174cfe6062c3f8c387f9b162e8aa926265c7b
Vincent <vincentiusmickey6@gmail.com>
2023-04-14 23:18:51 +0700

    Latihan 1 Aplikasi Komputasi Bergerak

---
+++ app/src/main/java/com/akb/latihan1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.akb.latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Tanggal Pengerjaan : 11/04/2023
+ * NIM : 10120909
+ * Nama : Vincentius Mickey Satya Wisesa
+ * Kelas : IF-10
+ * */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
